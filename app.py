091e538247734c1d9164ea390e786f1d89c9ef30
Fred Sheppard <frederickjssheppard@gmail.com>
2024-05-27 19:22:31 +0100

    Add error html

--- app.py
+++ app.py
@@ -6,11 +6,25 @@ from flask import request, Flask, jsonify
 app = Flask(__name__)
 
 
+@app.route('/')
+def home():
+    return """<h1>Welcome to Wind API</h1>
+Enter a request in the form 
+&lt;address&gt;/wind/?wind_speed=10&wind_angle=45&boat_speed=5"""
+
+
 @app.route('/wind/')
 def wind():
-    speed = float(request.args.get('wind_speed'))
-    angle = float(request.args.get('wind_angle'))
-    boat = float(request.args.get('boat_speed'))
+    # noinspection PyBroadException
+    try:
+        speed = float(request.args.get('wind_speed'))
+        angle = float(request.args.get('wind_angle'))
+        boat = float(request.args.get('boat_speed'))
+    except Exception:
+        return """
+<h1>GET request formatting error</h1>
+Enter a request in the form 
+&lt;address&gt;/wind/?wind_speed=10&wind_angle=45&boat_speed=5"""
 
     angle_rad = math.radians(angle)
     x = speed * math.cos(angle_rad)
@@ -21,8 +35,8 @@ def wind():
     true_speed = round(math.sqrt(true_x ** 2 + true_y ** 2), 2)
     true_angle = round(math.degrees(math.tan(true_y / true_x)), 2)
 
-    return jsonify({'speed': true_speed, 'angle': true_angle})
+    return jsonify({'true_speed': true_speed, 'true_angle': true_angle})
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
